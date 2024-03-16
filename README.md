d4b2df70bf5493800a32ec1426b2a00d3c2b3907
T3ODORO <lucasmine500@gmail.com>
2024-03-16 10:27:39 -0300

    aula2 teste 1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+teste
+
 # poo-01-2024
 Programação Orientada à Objeto em java.
--- exemplo/Exemplo.java
+++ exemplo/Exemplo.java
@@ -2,6 +2,6 @@ package exemplo;
 
 public class Exemplo {
    public static void main(String[] args){
-    System.out.println("Hello");
+    System.out.println("Hello world");
    }
 }
