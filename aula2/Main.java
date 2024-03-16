8e0e9d8f252b420ed91b54ce2a3215a5b5c1e6a6
T3ODORO <lucasmine500@gmail.com>
2024-03-16 11:10:28 -0300

    aula2 calculadora

--- aula2/Main.java
+++ aula2/Main.java
@@ -3,6 +3,19 @@ package aula2;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Olá mundo");
+
+        Calculadora calc = new Calculadora();
+
+        calc.numA = 2.0;
+        calc.numB = 3.0;
+
+        
+
+        System.out.println("Soma: " + calc.somar());
+        System.out.println("Subtração: " + calc.subitrair());
+        System.out.println("Multiplicação: " + calc.multiplicar());
+        System.out.println("Divisão: " + calc.dividir());
     }
+
+
 }
